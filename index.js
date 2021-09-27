2c53443943b9e01b02ed6500d3fc858e21ecdd09
Darrek Lau <Lau.kok.tiong@gmail.com>
2021-09-27 14:28:37 +0800

    change let to var in js function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function yrName() {
-  let people = prompt("What is your preferred name?");
+  var people = prompt("What is your preferred name?");
   document.getElementById("theNameHere").innerHTML = "Hello " + people +" !";
   alert("The name had been changed to " + people + ". Thank you !");
   console.log("The name has been change to " + people);
