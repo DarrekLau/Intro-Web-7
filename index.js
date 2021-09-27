4c6095c47439534196c9486c49d6132d2622f859
Darrek Lau <Lau.kok.tiong@gmail.com>
2021-09-27 13:36:07 +0800

    added an alert and con.log to show change

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function yrName(){
     let people= prompt("What is your preferred name?");
     document.getElementById("theNameHere").innerHTML= "Hello " + people; 
+    alert("The name had been changed to " + people + ". Thank you !");
+    console.log("The name has been change to " + people);
 }
